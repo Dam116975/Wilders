6f22b3a8b5cd5b42825b73d751232f87bf438614
Dam116975 <76938706+Dam116975@users.noreply.github.com>
2022-05-09 15:18:31 +0200

    dossier complet

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import chalk from 'chalk';
-console.log(chalk.blue('Rémigit '), chalk.red('Orel'), chalk.green('Elina'), chalk.grey('Thomas'));
+console.log(chalk.blue('Rémi'), chalk.red('Orel'), chalk.green('Elina'), chalk.grey('Thomas'));
 
 
